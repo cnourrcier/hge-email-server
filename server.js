90beaaa1f587ee9fd33127ad82c61dfe894611b4
Charles Nourrcier <cnourrcier@hotmail.com>
2025-04-21 10:41:31 -0400

    Add summit lodge

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const { sendEmail } = require('./sendGridEmailAPI');
 const { sendEmail } = require('./googleEmailAPI');
 
 const app = express();
@@ -12,9 +11,9 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: [
       'http://localhost:5173', 
-      'https://hiddengableestate.onrender.com',
       'https://hiddengableestate.com',
       'https://www.hiddengableestate.com',
+      'https://summitlodgebigbear.com',
   ]
 }));
 app.use(express.json());
